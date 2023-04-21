9e00860587fb2fb39b03c9c8832e6e89fa757c6c
Mike Young <mike.young@crowdbotics.com>
2023-04-21 15:37:21 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "tst-tc2698-ofjkdath-78292",
-  url: "https://tst-tc2698-ofjkdath-78292.botics.co",
-  api: "https://tst-tc2698-ofjkdath-78292.botics.co/api/v1"
-}
+  "name": "tst-tc2698-ofjkdath-78292",
+  "url": "https://tst-tc2698-ofjkdath-78292.botics.co",
+  "api": "https://tst-tc2698-ofjkdath-78292.botics.co/api/v1",
+  "initialRoute": "aboutTheApp"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
